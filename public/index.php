09415fc70dd5bd5f047b8bfb01885ae40caf4f36
eytorsousa <eytorlima@outlook.com>
2024-06-08 00:17:39 -0300

    adding main files & configs

--- public/index.php
+++ public/index.php
@@ -3,11 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com" defer></script>
-
+    
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'gcolor': '#475841',
+                        'lg-color': '#9FB8AD',
+                        'b-color': '#011627',
+                        'r-color': '#EFC7C2',
+                        'y-color': '#FFC800',
+                    }
+                }
+            }
+        }
+    </script>
     <title>Right Pass</title>
 </head>
 <body>
-    
 </body>
 </html>
