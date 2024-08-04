fdf13329ce5b2b66412121aca70f1577cbefbd00
ThiagoMRibeiro <77808417+ThiagoMoralesRibeiro@users.noreply.github.com>
2024-08-03 22:36:22 -0300

    Add files via upload

---
+++ app/controller/Login.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Model;
+
+use App\Model\User;
+use App\Services\Session;
+
+class Login{
+
+  protected $usermodel;
+
+
+  public function __construct(User $usermodel) {
+    $this->usermodel = $usermodel;
+  }
+
+  public function login($username, $password): bool{
+
+    $user = $this->usermodel->find("username = :name", "name=$username");
+
+    if($user != NULL && password_verify($password, $user->userpassword)){
+      Session::setSession('user_id', $user->id);
+      Session::setSession('is_logged', true);
+      return true;
+    }else{
+      return false;
+    }
+    
+  }
+
+  public function logout(){
+    Session::destroySession();
+  }
+}
+?>
